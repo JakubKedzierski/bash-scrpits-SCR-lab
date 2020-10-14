4a2bf6fe8645f2a0548607af4a0d207ce6fe8839
kuba <ikubai@wp.pl>
2020-10-14 23:38:38 +0200

    czwarty zaawansowany

--- lab2/czwarty.sh
+++ lab2/czwarty.sh
@@ -2,13 +2,15 @@
 
 usage(){ printf "Poprawne wywolanie: \n $0 [-n liczba dni modyfikacji] [-m maska] [-a nazwa awrchiwum] \n \n" ;}
 
-while getopts "n:ma" OPTION
+while getopts "n:m:a:" OPTION
 do 
 case $OPTION in
-n) echo n;;
-m) echo k;;
-a) echo a;;
+n) N=$OPTARG;;
+m) MASK=$OPTARG;;
+a) NAME=$OPTARG;;
 *) usage; exit 1;;
 esac
 done
+
+tar -cjf $NAME `find . -type f -name "$MASK" -mtime -$N`
 
