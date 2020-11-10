3a93493ef578bbbb0e6deeecfb590e8fffdb726f
kuba <ikubai@wp.pl>
2020-11-10 13:24:05 +0100

    Dodano zad2

--- lab4/first.c
+++ lab4/first.c
@@ -3,66 +3,73 @@
 #include <signal.h>
 #include <unistd.h>
 #include <ucontext.h>
+#include <stdlib.h>
 
-struct timespec ts;
+sigset_t sigset;
+int flag=0;
 
 void firstReaction(int sig, siginfo_t *sip, ucontext_t *uap)
 {
-	if(sig==SIGUSR1){
-		printf("Wyslano do mnie sygnal: %d. Zamykam sie. \n ", sig);
+
+	if(sig==SIGTERM){
+		printf("Wyslano do mnie sygnal: %d. Zamykam sie.\n ", sig);
 		exit(0);
 	}
+
+	if(sig==SIGUSR1){
+		printf("Wyslano do mnie sygnal: %d. Wracam do pracy.\n ", sig);
+	}
+
+	if(sig==SIGUSR2){
+		if(!flag){
+			printf("Wyslano do mnie sygnal: %d. Teraz sygnal bedzie niewidoczny przez 10 sekund.\n ", sig);
+		}else{
+			printf("Oczekiwal sygnal w kolejce. Domyslnie ignoruje. \n");
+			return;
+		}
+		flag=1;			// ustawia flage
+	}
+
+	if(sig==SIGALRM){}
+
 }
 
 int main()
 {
-	/*
-	sigset_t sigset;
-	sigemptyset(&sigset) ;
-	sigaddset(&sigset, SIGHUP);
-	sigprocmask(SIGINT,&sigset,NULL);
-	*/
-
-
+	sigemptyset(&sigset);
+	sigaddset(&sigset, SIGUSR2);  // dodaje SIGUSR1 do setu
+	
 	struct sigaction action;
 	action.sa_handler=(void*)firstReaction;  // przypisanie handlera
 	action.sa_flags = SA_SIGINFO;		
 
-	sigaction(SIGTERM, &action, 0);	
+	sigaction(SIGTERM, &action, 0);		// przypisanie akcji do sygnalow
 	sigaction(SIGUSR1, &action, 0);	
 	sigaction(SIGUSR2, &action, 0);	
 	sigaction(SIGALRM, &action, 0);	
 	
-	
-
-	ts.tv_nsec = 100 * 1000000L;
+	struct timespec ts;
+	ts.tv_nsec = 500000000L;
 	ts.tv_sec = 0;
 
-	int i = 0;
+	int i = 0, loopCounter=0;
+
 	while (1)
 	{
 		i++;
 		nanosleep(&ts, (struct timespec *)NULL);
+		if(flag){
+			if(!loopCounter) sigprocmask(SIG_BLOCK,&sigset,NULL);	// blokuje sygnal
+			
+			loopCounter++;
+			if(loopCounter>20){
+				printf("Jestem juz widoczny ! \n");
+				loopCounter=0;
+				sigprocmask(SIG_UNBLOCK,&sigset,NULL);
+				flag=!flag;
+			}
+		}
 	}
 }
 
-/*
-       If SA_SIGINFO is specified in sa_flags, then sa_sigaction (instead of
-       sa_handler) specifies the signal-handling function for signum.  This
-       function receives three arguments, as described below.
 
-       sa_mask specifies a mask of signals which should be blocked (i.e.,
-       added to the signal mask of the thread in which the signal handler is
-       invoked) during execution of the signal handler.  In addition, the
-       signal which triggered the handler will be blocked, unless the
-       SA_NODEFER flag is used.
-
-	int sigaction(int signum, const struct sigaction *act,struct sigaction *oldact);
-	int sigprocmask(int how, const sigset_t *set, sigset_t *oldset);
-
-	signum specifies the signal and can be any valid signal except
-    SIGKILL and SIGSTOP.
-
-	po ustawieniu sigprocmask nic nie przechodzi
-
-*/
