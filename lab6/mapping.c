8609ca947392f483f8f4ed9a54a53215de54316c
JakubKedzierski <248915@student.pwr.edu.pl>
2020-12-05 22:33:38 +0100

    poprawiono, teraz to ma wiekszy sens

--- lab6/mapping.c
+++ lab6/mapping.c
@@ -5,16 +5,27 @@
 #include <sys/mman.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <string.h>
 
 int main(){
-    int filefd=0,mapFileFd;
-    char *addr;
+
+    int mapFileFd,filefd;
+    char *addr,*fileMem,buff[1];
     struct stat fileStats;
     int count;
     char file_name[100];
+    int i=0;
 
-    if((mapFileFd=open("memory.txt",O_WRONLY)) < 0){
+    if((mapFileFd=open("memory.txt",O_RDWR)) < 0){
         fprintf(stderr,"Blad przy otwieraniu pliku do mapowania pamieci \n");
+        return -1;
+    }
+    count=lseek(mapFileFd,0,SEEK_END);
+
+    addr=mmap(NULL,count, PROT_WRITE , MAP_SHARED, mapFileFd, 0);
+
+    if (addr == MAP_FAILED){
+        fprintf(stderr,"Blad przy mapowaniu 1.  \n");
         return -1;
     }
 
@@ -31,19 +42,19 @@ int main(){
             fprintf(stderr,"Blad przy wyciaganiu statystyk.  \n");
             return -1;
         }  
+        
+        addr=mmap(NULL,fileStats.st_size, PROT_WRITE | PROT_READ , MAP_SHARED, mapFileFd, 0);
 
-        addr=mmap(NULL,fileStats.st_size, PROT_READ, MAP_PRIVATE, filefd, 0);
-        
-        if (addr == MAP_FAILED){
-            fprintf(stderr,"Blad przy mapowaniu.  \n");
-            return -1;
-        }
-        
+
         ftruncate(mapFileFd,fileStats.st_size);
         
-        if((count=write(mapFileFd,addr,fileStats.st_size))<0){
-            fprintf(stderr,"Blad przy wypiswaniu pamieci.  \n");
+        if (addr == MAP_FAILED){
+            fprintf(stderr,"Blad przy mapowaniu 2.  \n");
             return -1;
+        }
+
+        while((count=read(filefd,buff,1))>0){
+            addr[i++]=buff;
         }
     }
 
